3400765e91da3b9c5607f6bfb8504894a25d1d39
Jason Moore <jdavid.moore@mail.utoronto.ca>
2019-03-23 19:05:04 -0400

    Added user accounts and places and their managers.

---
+++ HackAThon/app/src/main/java/com/example/hackathon/Place.java
@@ -0,0 +1,26 @@
+package com.example.hackathon;
+
+import java.util.ArrayList;
+
+public class Place {
+
+    private String address;
+
+    //ArrayLists of reviews each will have a rating from 1 to 5 and a description
+    private ArrayList<String[]> reviews;
+
+    public Place(String address){
+        reviewsFromFile();
+        this.address = address;
+    }
+
+    public void addReview(User user, String message, int rating){
+        String[] review = {user.getUsername(), message, String.valueOf(rating)};
+        reviews.add(review);
+    }
+
+    public ArrayList<String[]> getReviews(){
+        return reviews;
+    }
+
+}
